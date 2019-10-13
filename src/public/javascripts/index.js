1da3449f8dfbb18ffbc490069fa657adb854106a
cnzmg <cnzmg@git.com>
2019-10-13 18:04:42 +0800

    push -m

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,18 +1,38 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ *
+ * One hundred thousand 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 import "../stylesheets/index.min.css";
 import json from "../json/conf.json";
 let xml = new XMLHttpRequest(),
     httpData = 'https://case.cbcoffee.cn/',
+    filePush = 'http://test.cbcoffee.cn:8085/upload_file',
+    assign = localStorage.getItem('token'),
     nextId = null,
     prevId = null,
+    index = null,
     _eq = "",
+    forShow = false,
     win = window, title = document.getElementsByClassName('title')[0],
     centent = document.getElementsByClassName('content-box')[0],
     next = document.getElementsByClassName('next')[0],
-    prev = document.getElementsByClassName('prev')[0]; //全局;
+    photo = document.getElementsByClassName('photo')[0],
+    prev = document.getElementsByClassName('prev')[0],
+    axios = require('axios'); //全局;
 
 document.addEventListener("DOMContentLoaded", function () {
     json.forEach($e => {
         if ($e.pageId == 0) {
+            index = 0;
             title.innerHTML = $e.question.title; //题目抬头
             $e.question.choice.forEach(($_, eq) => {  //选项列表
                 _eq += `<li> <input type="radio" name="choice" ${eq == 0 ? 'checked=checked' : ''} data-topid="${$_.topId}"  data-lastid="${$_.lastId}" id="${eq}"><label for="${eq}"> ${$_.key} </label></li>`
@@ -36,6 +56,8 @@ document.addEventListener("DOMContentLoaded", function () {
                     }) : null;
                 })
             })
+            prev.style.display = 'none';
+            next.style.width = '100%';
         };
         return false;
     })
@@ -58,7 +80,25 @@ document.addEventListener("DOMContentLoaded", function () {
             reader.onload = function (event) {
                 content = event.target.result;
                 compress(content, 450, function (content0) {
-                    console.log("final=" + content0.length / 1024 + "KB");
+                    // push image
+                    // axios.post(filePush, {
+                    //     userId: assign.userId,
+                    //     userToken: assign.userToken,
+                    //     file: content0,
+                    //   })
+                    //   .then(function (response) {
+                    //     console.log(response);
+                    //   })
+                    //   .catch(function (error) {
+                    //     console.log(error);
+                    //   });
+                    let _imgBox = document.createElement('figure'), _img = document.createElement('img'), _clone = document.createElement('i');
+                    _imgBox.className = 'hash[imageBox]';
+                    _img.src = content0;
+                    _imgBox.appendChild(_img);
+                    _imgBox.appendChild(_clone);
+                    photo.appendChild(_imgBox);
+                    
                 });
             };
             reader.onerror = function () {
@@ -88,23 +128,30 @@ function compress(content, size, callback) {  //压缩拍摄上传
         ctx.fillRect(0, 0, width, height);
         ctx.drawImage(img, 0, 0, width, height);
         let rate = (1024 * size) / content.length;
-        document.getElementsByClassName('old')[0].src = content;
         //进行压缩
         content = canvas.toDataURL("image/jpeg", 0.2);
-        document.getElementsByClassName('new')[0].src = content;
         callback(content);
     };
 }
 
 function getCententsPage(params) {  //填报进度
-    _eq = "";
+    _eq = "", index = params;
     json.forEach($e => {
         if (nextId && prevId) {
             if ($e.pageId == params) {
                 title.innerHTML = $e.question.title.replace('$', '<input class="_int_" type="number">'); //题目抬头
+                new RegExp("10|11|12|13|15|16|18|19|20|21|22").test($e.pageId) ? forShow = true : forShow = false;
                 $e.question.choice.forEach(($_, eq) => {  //选项列表
-                    _eq += `<li> <input type="radio" name="choice" ${eq == 0 ? 'checked=checked' : ''} data-topId="${$_.topId}"  data-lastId="${$_.lastId}" id="${eq}"><label for="${eq}"> ${$_.key.replace("$", '<input class="_int_" type="number">')} </label></li>`
+                    _eq += `<li> <input type="radio" name="choice" ${eq == 0 ? 'checked=checked' : ''}
+                    data-topId="${$_.topId}"  data-lastId="${$_.lastId}"
+                    id="${eq}">${!forShow ? `<label for="${eq}"> ${$_.key.replace("$", '<input class="_int_" type="number">')} </label>`
+                    : `<div> ${$_.key.replace("$", '<input class="_int_" type="number">')} </div>`} </li>`;
                 });
+                if($e.pageId == 3 || $e.pageId == 9 || $e.pageId == 12 || $e.pageId == 20){  //判断是否需要上传图片
+                    photo.style.display = 'block';
+                }else{
+                    photo.style.display = 'none';
+                }
                 centent.innerHTML = _eq;
                 document.querySelectorAll('input').forEach((name, index) => {
                     if (name.getAttribute('name') == 'choice') {
@@ -126,10 +173,23 @@ function getCententsPage(params) {  //填报进度
                 })
             };
         }
-    })
+    });
+    if(params == 0 || params == 22){
+        prev.style.display = 'none';
+        next.style.width = '100%';
+        params == 22 ? (()=>{
+            next.innerHTML = `完成`;
+            next.addEventListener('click', () =>{
+                WeixinJSBridge.call('closeWindow');
+            }, true)
+        })() : null;
+    }else{
+        prev.style.display = 'block';
+        next.style.width = '33.33%';
+    }
 }
 
-function checkedBox(params) {
+function checkedBox(params) {  //切换Page IDfBizJ8
     nextId = params.lastId;
     prevId = params.topId;
     next.setAttribute('data-value', nextId);
