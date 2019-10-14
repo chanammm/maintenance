aa567b6f50fd5486de828abb49a8baf3a8aad84b
cnzmg <cnzmg@git.com>
2019-10-14 18:17:13 +0800

    --

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -27,6 +27,7 @@ let xml = new XMLHttpRequest(),
     next = document.getElementsByClassName('next')[0],
     photo = document.getElementsByClassName('photo')[0],
     prev = document.getElementsByClassName('prev')[0],
+    loading = document.getElementsByClassName('module')[0],
     axios = require('axios'); //全局;
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -61,11 +62,13 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         return false;
     })
-    document.getElementsByClassName('next')[0].addEventListener('click', function (e) {
+    document.getElementsByClassName('next')[0].addEventListener('click', function (e) {  //下一步的操作
         getCententsPage(this.getAttribute('data-value'));
+
     });
-    document.getElementsByClassName('prev')[0].addEventListener('click', function (e) {
+    document.getElementsByClassName('prev')[0].addEventListener('click', function (e) {  //上一步的操作
         getCententsPage(this.getAttribute('data-value'));
+
     })
     // xml.open('GET', httpData + 'create_maintain_flow?userId=891&userToken=1a2039f53653e2ea8bdcbb4cbb7d69ba', false);
     // xml.send();
@@ -73,32 +76,56 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByClassName('push')[0].addEventListener('click', function (e) {  //上传图片
         document.getElementsByClassName('fileReader')[0].click();
         document.getElementsByClassName('fileReader')[0].onchange = function (e) {
+            loading.style.display = 'block';
             var localFile = this.files[0];
-            console.log(this.files[0])
             var reader = new FileReader();
             var content;
             reader.onload = function (event) {
                 content = event.target.result;
-                compress(content, 450, function (content0) {
+                compress(content, 450, function (contentFile) {
                     // push image
-                    // axios.post(filePush, {
-                    //     userId: assign.userId,
-                    //     userToken: assign.userToken,
-                    //     file: content0,
-                    //   })
-                    //   .then(function (response) {
-                    //     console.log(response);
-                    //   })
-                    //   .catch(function (error) {
-                    //     console.log(error);
-                    //   });
-                    let _imgBox = document.createElement('figure'), _img = document.createElement('img'), _clone = document.createElement('i');
-                    _imgBox.className = 'hash[imageBox]';
-                    _img.src = content0;
-                    _imgBox.appendChild(_img);
-                    _imgBox.appendChild(_clone);
-                    photo.appendChild(_imgBox);
-                    
+                    let _$file = new FormData();
+                    _$file.append('maintainerId', 41);
+                    _$file.append('type', 18);
+                    _$file.append('file', contentFile, 'cc.png');
+                    axios({
+                        method: "POST",
+                        url: filePush,
+                        data: _$file,
+                        processData: false, //必不可少参数
+                        traditional: true, //比不可少参数
+                        contentType: false,//比不可少参数
+                        headers: {
+                          "Content-Type": false
+                        //   headers: {'Content-Type':'application/x-www-form-urlencoded'}
+                        },
+                        onUploadProgress:function(progressEvent){ //原生获取上传进度的事件
+                            if(progressEvent.lengthComputable){
+                                //属性lengthComputable主要表明总共需要完成的工作量和已经完成的工作是否可以被测量
+                                //如果lengthComputable为false，就获取不到progressEvent.total和progressEvent.loaded
+                                if(progressEvent.total % progressEvent.loaded == +false){
+                                    setTimeout(()=>{
+                                        loading.style.display = 'none';
+                                    },2000)
+                                }
+                            }
+                        }
+                      }).then(
+                        response => {
+                            let _imgBox = document.createElement('figure'), _img = document.createElement('img'), _clone = document.createElement('svg'), _use = document.createElement('use');
+                            _imgBox.className = 'hash[imageBox]';
+                            _img.src = response.data.realPath;
+                            _imgBox.appendChild(_img);
+                            _clone.className = 'icon';
+                            _clone.setAttribute('aria-hidden', "true");
+                            _use.setAttribute('xlink:href', "#ym-icon-guanbi");
+                            _clone.appendChild(_use);
+                            _imgBox.appendChild(_clone);
+                            photo.appendChild(_imgBox);
+                        }
+                      ).catch((error) => {
+                        console.log(error);
+                      })
                 });
             };
             reader.onerror = function () {
@@ -111,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function compress(content, size, callback) {  //压缩拍摄上传
     if (content.length <= size * 1024) {
-        callback(content);
+        callback(dataURItoBlob(content));
         return;
     }
     let canvas = document.createElement("canvas");
@@ -128,11 +155,33 @@ function compress(content, size, callback) {  //压缩拍摄上传
         ctx.fillRect(0, 0, width, height);
         ctx.drawImage(img, 0, 0, width, height);
         let rate = (1024 * size) / content.length;
+        console.log(content.length * 1024);
         //进行压缩
         content = canvas.toDataURL("image/jpeg", 0.2);
-        callback(content);
+        console.log(content.length * 1024);
+        let blob = dataURItoBlob(content);
+        callback(blob);
     };
 }
+/**
+ * base64 转二进制文件
+ * @param {*} base64Data 
+ */
+function dataURItoBlob(base64Data) {
+    var bytes = window.atob(base64Data.split(',')[1]); //去掉url的头，并转换为byte
+
+    //处理异常,将ascii码小于0的转换为大于0
+    var ab = new ArrayBuffer(bytes.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < bytes.length; i++) {
+        ia[i] = bytes.charCodeAt(i);
+    }
+
+    return new Blob([ab], {
+        type: 'image/png'
+    });
+}
+
 
 function getCententsPage(params) {  //填报进度
     _eq = "", index = params;
@@ -174,7 +223,7 @@ function getCententsPage(params) {  //填报进度
             };
         }
     });
-    if(params == 0 || params == 22){
+    if(params == 0 || params == 22){   //进度不同的按钮文字提示
         prev.style.display = 'none';
         next.style.width = '100%';
         params == 22 ? (()=>{
@@ -189,7 +238,7 @@ function getCententsPage(params) {  //填报进度
     }
 }
 
-function checkedBox(params) {  //切换Page IDfBizJ8
+function checkedBox(params) {  //切换选择项目的任务继续Page ID fBizJ8
     nextId = params.lastId;
     prevId = params.topId;
     next.setAttribute('data-value', nextId);
